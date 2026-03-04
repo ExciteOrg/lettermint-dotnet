2fc765c8ff53afc90f696950a917b7e9e4ff51de
Lars Bloch Jensen <lars@gmail.com>
2026-03-04 19:55:20 +0100

    Added json properties to the email respone to correctly map the lettermint json

--- lettermint-dotnet/Models/EmailResponse.cs
+++ lettermint-dotnet/Models/EmailResponse.cs
@@ -1,6 +1,10 @@
-﻿namespace Lettermint.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Lettermint.Models;
 public class EmailResponse
 {
+    [JsonPropertyName("message_id")]
     public string MessageId { get; set; } = string.Empty;
+    [JsonPropertyName("status")]
     public string Status { get; set; } = string.Empty;
 }
